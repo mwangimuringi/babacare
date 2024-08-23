f54ad747c7288ec747563c06e5d7c9279643d055
mwangimuringi <mwangimuringi10@gmail.com>
2024-08-23 13:27:52 +0300

    Updated global css and tailwind config

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-3xl underline">
+    <div className="text-white text-3xl underline">
       <h1>Hello world</h1>
     </div>
   );
