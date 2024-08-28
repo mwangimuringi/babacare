449b21f55eb50b834266b642c71f529c4eff1193
mwangimuringi <mwangimuringi10@gmail.com>
2024-08-28 11:11:32 +0300

    Added the admin passkey page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
-import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+import PatientForm from "@/components/forms/PatientForm";
+import { SearchParamProps } from "@/types/index.d";
+import { PasskeyModal } from "@/components/PasskeyModal";
+
+const Home = ({ searchParams }: SearchParamProps) => {
+  const isAdmin = searchParams?.admin === "true";
   return (
-    <div  className="flex h-screen max-h-screen">
+    <div className="flex h-screen max-h-screen">
+      {/* TODO: OTP verification | PasskeyModal*/}
+      {isAdmin && <PasskeyModal />}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -36,4 +42,5 @@ export default function Home() {
       />
     </div>
   );
-}
+};
+export default Home;
