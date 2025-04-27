edc85b240ebfb1ddd40d022a7120f80d72cae53b
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-27 23:54:34 +0300

    Initialized formData state for FeedbackForm

--- components/forms/FeedbackForm.tsx
+++ components/forms/FeedbackForm.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const FeedbackForm = () => {
+  const [formData, setFormData] = useState({
+    rating: '',
+    comments: ''
+  });
+
   return (
     <form>
       <h2>Feedback Form</h2>
       <label>Rating (1-5):</label>
-      <input type="number" name="rating" min="1" max="5" required />
-      
+      <input type="number" name="rating" required />
+
       <label>Comments:</label>
       <textarea name="comments" required />
 
