f5e59763ab1383b5aba3f247f6f942eb531bb800
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-30 23:57:51 +0300

    Improve layout and spacing

--- components/ConfirmationScreen.tsx
+++ components/ConfirmationScreen.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router';
+import { Props } from 'next/script';
 
 const ConfirmationScreen: React.FC<Props> = ({ amount, cardEnding, date }) => {
   const router = useRouter();
