8e895f49c358971406a0945d54a537eea2cb149a
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-30 23:51:33 +0300

    Added thank you message and wrapper

--- components/ConfirmationScreen.tsx
+++ components/ConfirmationScreen.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const ConfirmationScreen = () => {
-  return <div>Confirmation Screen</div>;
-};
+    return (
+      <div className="p-4 text-center">
+        <h1 className="text-3xl font-bold">Thank You!</h1>
+        <p>Your payment was successful.</p>
+      </div>
+    );
+  };  
 
 export default ConfirmationScreen;
