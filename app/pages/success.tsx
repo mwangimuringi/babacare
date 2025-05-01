8ca270c7e4223fe1030c57e987e162a6bc922e5b
mwangimuringi <mwangimuringi10@gmail.com>
2025-05-01 23:52:44 +0300

    Wrap content in a container

--- app/pages/success.tsx
+++ app/pages/success.tsx
@@ -3,7 +3,6 @@ import ConfirmationScreen from "@/components/ConfirmationScreen";
 
 const SuccessPage = () => {
   return (
-    <>
     <div className="flex flex-col min-h-screen justify-center items-center">
       <Head>
         <title>Payment Success - Babacare</title>
@@ -13,8 +12,7 @@ const SuccessPage = () => {
         cardEnding="1234"
         date={new Date().toLocaleDateString()}
       />
-      </div>
-    </>
+    </div>
   );
 };
 
