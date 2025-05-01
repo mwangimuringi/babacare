60de6594c9393d5f9ccb7eb8c8b64ad6cf12d69b
mwangimuringi <mwangimuringi10@gmail.com>
2025-05-01 23:50:17 +0300

    ...

--- app/pages/success.tsx
+++ app/pages/success.tsx
@@ -1,12 +1,19 @@
+import Head from 'next/head';
 import ConfirmationScreen from '@/components/ConfirmationScreen';
 
 const SuccessPage = () => {
     return (
-        <ConfirmationScreen
+        <>
+  <Head>
+    <title>Payment Success - Babacare</title>
+  </Head>
+  <ConfirmationScreen
           amount={120}
           cardEnding="1234"
           date={new Date().toLocaleDateString()}
         />
+</>
+        
       );      
 };
 
