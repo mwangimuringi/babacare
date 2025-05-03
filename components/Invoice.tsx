94e48d484eacab0b1f7bcd8ddd70f24d26a3faaf
mwangimuringi <mwangimuringi10@gmail.com>
2025-05-03 23:50:28 +0300

    Added props interface

--- components/Invoice.tsx
+++ components/Invoice.tsx
@@ -1,3 +1,16 @@
+type InvoiceItem = {
+    description: string;
+    quantity: number;
+    price: number;
+  };
+  
+  type InvoiceProps = {
+    invoiceNumber: string;
+    customerName: string;
+    date: string;
+    items: InvoiceItem[];
+  };  
+
 const Invoice = () => {
     return <div>Invoice Component</div>;
   };
