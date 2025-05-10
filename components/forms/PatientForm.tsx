d316a9221b0a4baa4b4e5701bafd386fcc6e3450
mwangimuringi <mwangimuringi10@gmail.com>
2025-05-10 23:56:02 +0300

    Pass query params as props

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -63,6 +63,7 @@ export const PatientForm = () => {
       <form onSubmit={form.handleSubmit(onSubmit)} className="flex-1 space-y-6">
         <section className="mb-12 space-y-4">
           <h1 className="header">Hi there 👋</h1>
+          <p className="text-dark-700"></p>
           <p className="text-dark-700">Get started with appointments.</p>
         </section>
 
