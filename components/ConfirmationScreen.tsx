9e20dceb1c170c1d309b47d31fec173abceb3116
mwangimuringi <mwangimuringi10@gmail.com>
2025-05-10 23:21:04 +0300

    ...

--- components/ConfirmationScreen.tsx
+++ components/ConfirmationScreen.tsx
@@ -32,6 +32,8 @@ const ConfirmationScreen: React.FC<Props> = ({ amount, cardEnding, date }) => {
       </div>
 
       <button
+
+      
         onClick={() => router.push('/')}
         className="mt-6 px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition"
       >
