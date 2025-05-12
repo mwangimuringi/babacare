8c4e47daee7397c10c424238051d2d332cdcdb2b
mwangimuringi <mwangimuringi10@gmail.com>
2025-05-12 22:49:41 +0300

    Display itemized table of purchased items

--- app/patients/[userid]/new-appointment/page.tsx
+++ app/patients/[userid]/new-appointment/page.tsx
@@ -19,6 +19,7 @@ const Appointment = async ({ params: { userId } }: SearchParamProps) => {
             alt="logo"
             className="mb-12 h-10 w-fit"
           />
+          
 
           <AppointmentForm
             patientId={patient?.$id}
