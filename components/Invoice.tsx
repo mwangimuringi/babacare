05f5cc0c21fcb895b4f5112f543438d67c41bbdd
mwangimuringi <mwangimuringi10@gmail.com>
2025-05-19 23:52:49 +0300

    Improve error fallback logic

--- components/Invoice.tsx
+++ components/Invoice.tsx
@@ -65,9 +65,6 @@ const Invoice: React.FC<InvoiceProps> = ({
               <td className="px-4 py-2 border">
                 {formatCurrency(item.price)}
               </td>
-              <td className="px-4 py-2 border">
-                {formatCurrency(item.quantity * item.price)}
-              </td>
             </tr>
           {items.map((item, index) => (
             <tr key={index}>
