94464bbc19a49faf62fb1b7ff0a40bc99395095e
mwangimuringi <mwangimuringi10@gmail.com>
2025-05-23 23:58:23 +0300

    Added section headings for better readability

--- components/Receipt.tsx
+++ components/Receipt.tsx
@@ -36,6 +36,10 @@ export default function Receipt({
 
       <hr className="my-4" />
 
+      <p className="text-sm">Items</p>
+      <hr className="my-2" />
+      <p className="text-sm font-bold">Subtotal</p>
+
       <table className="w-full text-sm mt-4 border-t pt-2">
         <thead>
           <tr>
