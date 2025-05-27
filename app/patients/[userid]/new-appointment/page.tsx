90c538991cbd719ce2229d0995b37af30e4f0ba9
mwangimuringi <mwangimuringi10@gmail.com>
2025-05-27 23:57:29 +0300

    Styled well using tailwind css

--- app/patients/[userid]/new-appointment/page.tsx
+++ app/patients/[userid]/new-appointment/page.tsx
@@ -36,37 +36,6 @@ const Appointment = async ({ params: { userId } }: SearchParamProps) => {
               className="search-input"
             />
           </div>
-
-          <div className="profile-container">
-            
-          </div>
-        <div className="sub-container max-w-[860px] flex-1 justify-between">
-          <Image
-            src={`/assets/icons/logo-full.svg`}
-            height={1000}
-            width={1000}
-            alt="logo"
-            className="mb-12 h-10 w-fit"
-          />
-
-          <AppointmentForm
-            patientId={patient?.$id}
-            userId={userId}
-            type="create"
-          />
-
-          <p className="copyright mt-10 py-12">© 2024 CarePluse</p>
-        </div>
-      </section>
-
-      <Image
-        src={`/assets/images/appointment-img.png`}
-        height={1500}
-        width={1500}
-        alt="appointment"
-        className="side-img max-w-[390px] bg-bottom"
-      />
-    </div>
   );
 };
 
