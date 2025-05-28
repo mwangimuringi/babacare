cf82d07cc2193c44890cbdab11e77d2380c6928c
mwangimuringi <mwangimuringi10@gmail.com>
2025-05-28 23:58:44 +0300

    Improve error fallback logic

--- app/patients/[userid]/new-appointment/page.tsx
+++ app/patients/[userid]/new-appointment/page.tsx
@@ -9,3 +9,10 @@ import { useFetcher as useFetcher3 } from "@remix-run/react";
 import { useFetcher as useFetcher4 } from "@remix-run/react";
 import { useFetcher as useFetcher5 } from "@remix-run/react";
 import { useFetcher as useFetcher6 } from "@remix-run/react";
+
+
+
+
+readonly fetcher = useFetcher();
+readonly fetcher2 = useFetcher2();
+readonly fetcher3 = useFetcher3();
