b72e8ffc34f498a933e9876df8d8e77fcd6fc232
mwangimuringi <mwangimuringi10@gmail.com>
2025-05-29 23:55:34 +0300

    Formatted prices as currency

--- app/patients/[userid]/new-appointment/page.tsx
+++ app/patients/[userid]/new-appointment/page.tsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 
 const NewAppointmentPage = () => {
+  const [patientId, setPatientId] = useState<string>("");
+  const router = useRouter();
+
+  useEffect(() => {
+    const patientId = router.query.patientId as string;
+    setPatientId(patientId);
+  }, [router.query.patientId]);
   return <div>New Appointment Page</div>;
 };
 
