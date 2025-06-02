a58a3174832351aa76abf0436cd765fdfddbacac
mwangimuringi <mwangimuringi10@gmail.com>
2025-06-02 23:58:49 +0300

    Pass query params as props

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -82,7 +82,7 @@ export const AppointmentForm = ({
     values: z.infer<typeof AppointmentFormValidation>
   ): Promise<void> => {
     const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    const status = determineStatus(type); 
+    const status = determineStatus(type);
 
     try {
       if (type === "create" && patientId) {
@@ -136,8 +136,6 @@ export const AppointmentForm = ({
     }
   };
 
-  
-
   const renderFormFields = () => {
     if (type === "cancel") {
       return (
@@ -186,8 +184,9 @@ export const AppointmentForm = ({
         />
 
         <div
-          className={`flex flex-col gap-6 ${type === "create" && "xl:flex-row"
-            }`}
+          className={`flex flex-col gap-6 ${
+            type === "create" && "xl:flex-row"
+          }`}
         >
           <CustomFormField
             fieldType={FormFieldType.TEXTAREA}
