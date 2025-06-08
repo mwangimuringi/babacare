bc192db4ab6ffa370038c324586e33b5b3ce6f1a
mwangimuringi <mwangimuringi10@gmail.com>
2025-06-08 22:59:09 +0300

    Styled table with Tailwind.

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -42,7 +42,6 @@ const RegisterForm = ({ user }: { user: User }) => {
   };
 
   form.register("phone", { required: true, validate: validatePhone });
-  form.register("email", { required: true, validate: validateEmail });
   form.register("name", { required: true });
   // TODO: Add validation for email format
  
