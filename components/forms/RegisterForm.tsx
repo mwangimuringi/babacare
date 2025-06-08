da8d2d9651ba9cd3d8b2eb3cd1cc58e53e9e16e9
mwangimuringi <mwangimuringi10@gmail.com>
2025-06-08 23:11:06 +0300

    Styled table with Tailwind.

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -74,25 +74,5 @@ const RegisterForm = ({ user }: { user: User }) => {
     }
   };
 
-  return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="flex-1 space-y-12"
-      >
-        <section className="space-y-4">
-          <h1 className="header">Welcome 👋</h1>
-          <p className="text-dark-700">Let us know more about yourself.</p>
-        </section>
-
-        <PersonalInformationSection control={form.control} />
-        <MedicalInformationSection control={form.control} />
-        <IdentificationSection control={form.control} />
-        <ProfileImageUploader control={form.control} />
-        <SubmitSection isLoading={isLoading} />
-      </form>
-    </Form>
-  );
-};
 
 export default RegisterForm;
