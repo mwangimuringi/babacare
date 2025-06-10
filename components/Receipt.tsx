da65c151915f1f4db83d1c4674effda3650dba37
mwangimuringi <mwangimuringi10@gmail.com>
2025-06-10 23:57:05 +0300

    Rendered ConfirmationScreen inside SuccessPage

--- components/Receipt.tsx
+++ components/Receipt.tsx
@@ -82,6 +82,12 @@ export default function Receipt({
 
       <button className="mt-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition">
         Download PDF
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="ml-2 h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        ></svg>
       </button>
 
       <hr className="mt-4" />
