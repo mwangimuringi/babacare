deff25ad4bcf3413b477295c1b62a71081f79882
mwangimuringi <mwangimuringi10@gmail.com>
2025-06-22 22:36:58 +0300

    Added company and customer info

--- instrumentation.ts
+++ instrumentation.ts
@@ -12,7 +12,12 @@ export async function setupOnce() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('./sentry.server.config');
   }
+
   if (process.env.NEXT_RUNTIME === 'edge') {
     await import('./sentry.edge.config');
   }
+
+  // TODO: add other setups
+  // ...
+  
 }
