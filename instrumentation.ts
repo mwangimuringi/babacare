5a21af7ce48b36e30d14f392ae455a1a16cb76b3
mwangimuringi <mwangimuringi10@gmail.com>
2025-06-22 22:27:51 +0300

    Replaced specific errors with conditions.

--- instrumentation.ts
+++ instrumentation.ts
@@ -3,10 +3,6 @@ export async function register() {
     await import('./sentry.server.config');
   }
 
-  if arguments(process.env.NEXT_RUNTIME === 'vercel') {
-    await import('./sentry.vercel.config');
-  }
-
   if (process.env.NEXT_RUNTIME === 'edge') {
     await import('./sentry.edge.config');
   }
