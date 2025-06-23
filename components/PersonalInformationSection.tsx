521f2a8197ffe1858e7a951597cb18613c2f5572
mwangimuringi <mwangimuringi10@gmail.com>
2025-06-23 23:43:14 +0300

    ....

--- components/PersonalInformationSection.tsx
+++ components/PersonalInformationSection.tsx
@@ -6,7 +6,11 @@ import RadioGroupField from "@/components/RadioGroupField";
 import { GenderOptions } from "@/constants";
 import { Label } from "@/components/ui/label";
 
-const PersonalInformationSection = ({ control }: { control: Control<FormData> }) => (
+const PersonalInformationSection = ({
+  control,
+}: {
+  control: Control<FormData>;
+}) => (
   <section className="space-y-6">
     <div className="mb-9 space-y-1">
       <h2 className="sub-header">Personal Information</h2>
@@ -14,25 +18,47 @@ const PersonalInformationSection = ({ control }: { control: Control<FormData> })
 
     <div>
       <Label htmlFor="name">Name</Label>
-      <CustomFormField id="name" fieldType={FormFieldType.INPUT} control={control} name="name" placeholder="John Doe" />
+      <CustomFormField
+        id="name"
+        fieldType={FormFieldType.INPUT}
+        control={control}
+        name="name"
+        placeholder="John Doe"
+      />
     </div>
 
-  
     <div className="flex flex-col gap-6 xl:flex-row">
       <div>
         <Label htmlFor="email">Email address</Label>
-        <CustomFormField id="email" fieldType={FormFieldType.INPUT} control={control} name="email" placeholder="johndoe@gmail.com" />
+        <CustomFormField
+          id="email"
+          fieldType={FormFieldType.INPUT}
+          control={control}
+          name="email"
+          placeholder="johndoe@gmail.com"
+        />
       </div>
       <div>
         <Label htmlFor="phone">Phone Number</Label>
-        <CustomFormField id="phone" fieldType={FormFieldType.PHONE_INPUT} control={control} name="phone" placeholder="(555) 123-4567" />
+        <CustomFormField
+          id="phone"
+          fieldType={FormFieldType.PHONE_INPUT}
+          control={control}
+          name="phone"
+          placeholder="(555) 123-4567"
+        />
       </div>
     </div>
 
     <div className="flex flex-col gap-6 xl:flex-row">
       <div>
         <Label htmlFor="birthDate">Date of birth</Label>
-        <CustomFormField id="birthDate" fieldType={FormFieldType.DATE_PICKER} control={control} name="birthDate" />
+        <CustomFormField
+          id="birthDate"
+          fieldType={FormFieldType.DATE_PICKER}
+          control={control}
+          name="birthDate"
+        />
       </div>
       <div>
         <Label htmlFor="gender">Gender</Label>
